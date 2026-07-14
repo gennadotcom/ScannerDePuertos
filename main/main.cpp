8587327aee3307e6d031a3856a55d8a8986e7324
gennadotcom <genarofattore875@gmail.com>
2026-07-14 16:02:32 -0300

    Implementar logica del menu, ordenamiento y busqueda binaria
    - Se implementó el menú interactivo con un bucle do-while y switch.
    - Se agregó la función `escanearPuertos` para simular la carga de puertos.
    - Se implementó el algoritmo de ordenamiento Burbuja para el reporte de puertos.
    - Se añadió búsqueda binaria en la función `buscarPuerto`.
    - Se corrigieron desbordamientos de memoria en los límites de los bucles.

--- main/main.cpp
+++ main/main.cpp
@@ -1,12 +1,17 @@
 #include <stdio.h>
+//#include <winsock.h>
+//#include <ws2tcpip.h>
+
+//#pragma comment(lib, "ws2_32.lib")
 
 
-const int MAXPUERTOS = 20;
+const int MAXPUERTOS = 1023;
 
 void mostrarMenu(int *res);
-void escanearPuertos();
-void ordenamientoBurbuja();
-void mostrarEscaneo();
+void escanearPuertos(int array[], int * cant, int inicio, int fin);
+void ordenamientoBurbuja(int array[], int n);
+void mostrarEscaneo(int array[], int n);
+int buscarPuerto(int array[], int n, int puerto);
 
 
 
@@ -14,7 +19,7 @@ int main()
 {
 	int puertosEscaneados[MAXPUERTOS];
 	int cantActual = 0; //cantidad de puertos escaneados
-	int opcion;
+	int opcion, inicio, fin, puertoAbuscar;
 
 
 	
@@ -27,9 +32,14 @@ int main()
 		{
 
 		case 1:
+			printf("donde desea iniciar el escaneo?");
+			scanf_s("%d", &inicio);
 
-			printf("Iniciando escaneo...\n");
-			//escanearPuertos();
+			printf("donde desea finzalizar el escaneo?");
+			scanf_s("%d", &fin);
+
+			printf("[INFO]Iniciando escaneo...\n");
+			escanearPuertos(puertosEscaneados, &cantActual, inicio, fin);
 			printf("Escaneo finalizado.\n");
 			break;
 
@@ -39,10 +49,10 @@ int main()
 			else
 			{
 				printf("-----Reporte de escaneo-----\n");
-				//ordenamientoBurbuja();
-				//ordenamientoBurbuja();
-				break;
+				mostrarEscaneo(puertosEscaneados, cantActual);
+				
 			}
+			break;
 
 		case 3:
 
@@ -50,19 +60,29 @@ int main()
 			else
 			{
 				printf("-----Buscar puerto-----\n");
-				//ordenamientoBurbuja();
-				break;
-			}
+				printf("-Puerto a buscar: ");
+				scanf_s("%d", &puertoAbuscar);
 
+				int resultado = buscarPuerto(puertosEscaneados, cantActual, puertoAbuscar);
+				printf("[INFO] El puerto se ha encontrado en la posicion %d \n", resultado);
+
+				if(resultado == -1 ) printf("[INFO] No se ha encontrado el puerto.\n");
+			}
+			break;
+		case 4:
+			printf("Saliendo del programa... \n");
+			printf("Adios!");
+			break;
 		default:
 
-			printf("Opcion no valida. Introduzca una opcion valida.\n");
+			printf("Opcion no valida! Introduzca una opcion valida.\n");
 			break;
 
 		}
 
 	} while (opcion != 4);
 
+	//WSACleanup();
 	return 0;
 }
 
@@ -81,16 +101,76 @@ void mostrarMenu(int *res)
 	*res = respuesta;
 }
 
-void escanearPuertos()
+void escanearPuertos(int array[], int *cant, int inicio, int fin)
 {
+	
+	
+	for ( int i = inicio; i < fin + 1; i++)
+	{
+		array[i-inicio] = i ;
+	}
+	*cant = (fin - inicio) + 1;
+
+	printf("[INFO] Se encontraron %d puertos abiertos\n", *cant);
+
+
 }
 
-void ordenamientoBurbuja()
+void ordenamientoBurbuja(int array[], int n)
 {
+	int i, j, temp;
 
+	for (i = 0; i < n - 1; i++) 
+	{
+
+		for (j = 0; j < n - i - 1; j++) 
+		{
+
+			if (array[j] > array[j + 1]) 
+			{
+				
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+		}
+	}
 }
 
-void mostrarEscaneo()
+void mostrarEscaneo(int array[], int n)
 {
 
+	ordenamientoBurbuja(array, n);
+
+	for (int i = 0; i < n; i++)
+	{
+		printf(" %d ", array[i]);
+	}
+}
+
+int buscarPuerto(int array[], int n, int puerto) 
+{
+	int derecha = n - 1;
+	int izquierda = 0;
+	int medio;
+	
+	while (izquierda <= derecha)
+	{
+		medio = (izquierda + derecha) / 2;
+		if (array[medio] == puerto)
+		{
+			return medio;
+		}
+		if (puerto > array[medio])
+		{
+			izquierda = medio + 1;
+		}
+		else
+		{
+			derecha = medio - 1;
+		}
+	}
+
+
+	return -1;
 }
